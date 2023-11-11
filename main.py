6166fcfd15adda3e61706ec883e1fa217ebb7f96
wsekula <01159453@pw.edu.pl>
2023-11-11 18:51:07 +0100

    clean up code

--- main.py
+++ main.py
@@ -8,36 +8,19 @@ conn_str = (
     r'PWD=alfanumeryczne1@;'
 )
 
-# Replace with your database file path
-db_file = "pis2023z.database.windows.net"
-
-
-
-# SQL statement to select all records from the table
-select_records_sql = """
-SELECT * FROM sample_table
-"""
-
 try:
     # Connect to the SQLite database
     connection = pyodbc.connect(conn_str)
-
     cursor = connection.cursor()
 
-    # Execute the SQL statement to select all records
-    cursor.execute("CREATE TABLE new_tab (id INT, name VARCHAR(20))")
-    cursor.execute("INSERT INTO new_tab VALUES (1, 'Chris'), (2, 'John'), (3, 'Mike')")
     cursor.execute("SELECT * FROM new_tab")
     # Fetch all the records
     records = cursor.fetchall()
     connection.commit()
 
-
-    # Print the retrieved records
     for record in records:
         print(record)
 
-    # Don't forget to close the cursor and connection when done
     cursor.close()
     connection.close()
     print("Connection closed.")
