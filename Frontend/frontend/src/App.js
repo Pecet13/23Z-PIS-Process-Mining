ca722ee505f0393fbb6e8bec21b636f040de8f28
wojte <wojteksekua@gmail.com>
2023-11-13 09:26:48 +0100

    changed dir names and added frontend hello world app

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import logo from './logo.svg';
+
+function App() {
+  return (
+      <div className = 'App'><header className = 'App-header'>
+      <img src = {logo} className = 'App-logo' alt = 'logo' /><div><h1>Hello,
+      World!</h1>
+        </div></header>
+    </div>);
+}
+
+export default App;
