6f6382c98e872dbebdb856fb742bb734a46b2e66
wojte <wojteksekua@gmail.com>
2023-12-12 16:45:04 +0100

    added kafka consumer

---
+++ KafkaConsumer/kafka_consumer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+import json
+import requests  # If you're using HTTP requests to send data to your app
+
+# Set up the Kafka consumer
+consumer = KafkaConsumer(
+    'incident-logs-IncidentLogs',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    group_id='my-group'
+)
+
+# Continuously listen for messages
+for message in consumer:
+    data = json.loads(message.value)
+    response = requests.post('https://pis2023-pm.azurewebsites.net/api/endpoint', json=data)
